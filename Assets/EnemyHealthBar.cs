b1a76d26d0912377ecac8310987a82d8c4c356cc
Danielle Anderson <danielle.anderson2@marist.edu>
2020-03-09 21:46:44 -0400

    Added an enemy health bar that is set above their heads

---
+++ Assets/EnemyHealthBar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealthBar : MonoBehaviour
+{
+	public GameObject enemy;
+	private int healthLeft;
+	private Vector2 pos;
+	private Vector2 point;
+
+    // Update is called once per frame
+    void Update()
+    {
+    	//if enemy is destroyed destory the healthbar as well
+    	if (this.GetComponent<Slider>().value == 0)
+    	{
+    		Destroy(this.gameObject);
+    	}
+
+    	//set position of health bar above enemy's head at all times
+    	point = new Vector2(enemy.transform.position.x, enemy.transform.position.y + 1f);
+    	pos = Camera.main.WorldToScreenPoint (point);
+        transform.position = pos;
+
+        //set the value of the bar to enemy's health
+        healthLeft = enemy.GetComponent<EnemyControl>().enemyHealth;
+        this.GetComponent<Slider>().value = healthLeft;
+    }
+}
